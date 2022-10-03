fadf730746039e38f556dbf5ecbbfe5bc54547e1
osm3rr <osm3rr@gmail.com>
2022-10-02 22:58:39 -0400

    boundaries added

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ pygame.init()
 # 2. colors variables
 black = ( 0, 0, 0 )
 white = ( 255, 255, 255 )
+light_grey = ( 200, 200, 200 )
 
 # 3. screen size
-screen_size = ( 800,600 )
+screen_width = 800
+screen_height = 600
+screen_size = ( screen_width, screen_height )
 
 # 11. define the player constants
 player_width = 15
@@ -31,6 +34,10 @@ ball_radius = 10
 ball_speed_x = 5
 ball_speed_y = 5
 
+# function section
+def ball_restart():
+    ball_x_coord = screen_width / 2
+    ball_y_coord = screen_height / 2
 
 # 4. create the windows
 screen = pygame.display.set_mode( screen_size )
@@ -78,22 +85,34 @@ while not game_over:
             if event.key == pygame.K_DOWN:
                 player_2_y_speed = 0
 
-    # ball limits
-    if ball_y_coord > 590 or ball_y_coord < 10:
+    # ball boundary: top or bottom
+    if ball_y_coord > (screen_height - ball_radius) or ball_y_coord < ball_radius:
         ball_speed_y *= -1
 
-    # ball limit: right or left
-    if (ball_x_coord > 800) or (ball_x_coord < 0):
-        ball_x_coord = 400
-        ball_y_coord = 300
-
+    # ball boundary: right or left
+    if (ball_x_coord > screen_width) or (ball_x_coord < 0):
         ball_speed_x *= -1
-        ball_speed_y *= -1
-    
+        
+          
 
     # players and ball movement
     player_1_y_coord += player_1_y_speed
     player_2_y_coord += player_2_y_speed
+
+    # player 1 boundary
+    if player_1_y_coord <= 0:
+        player_1_y_coord = 0
+
+    if player_1_y_coord >= (screen_height - player_height):
+        player_1_y_coord = screen_height - player_height
+
+    # player 2 boundary
+    if player_2_y_coord <= 0:
+        player_2_y_coord = 0
+
+    if player_2_y_coord >= (screen_height - player_height):
+        player_2_y_coord = screen_height - player_height
+    
     # ball movement
     ball_x_coord += ball_speed_x
     ball_y_coord += ball_speed_y
@@ -104,13 +123,16 @@ while not game_over:
     # 9. drawing area
     
     # 10. define the player 1- left: a rectangle
-    player_1 = pygame.draw.rect( screen, white, ( player_1_x_coord, player_1_y_coord, player_width, player_height ) )
+    player_1 = pygame.draw.rect( screen, light_grey, ( player_1_x_coord, player_1_y_coord, player_width, player_height ) )
 
     # 10. define the player 2 - right
-    player_2 = pygame.draw.rect( screen, white, ( player_2_x_coord, player_2_y_coord, player_width, player_height ) )
+    player_2 = pygame.draw.rect( screen, light_grey, ( player_2_x_coord, player_2_y_coord, player_width, player_height ) )
 
     # 12. draw the ball
-    ball = pygame.draw.circle( screen, white, ( ball_x_coord, ball_y_coord ), ball_radius )
+    ball = pygame.draw.circle( screen, light_grey, ( ball_x_coord, ball_y_coord ), ball_radius )
+    
+    # 14. center line
+    pygame.draw.aaline( screen, light_grey, ( screen_width/2 ,0 ), ( screen_width/2, screen_height ) )
 
     # 13. collisions
     if ball.colliderect( player_1 ) or ball.colliderect( player_2 ):
@@ -121,4 +143,4 @@ while not game_over:
     
     # 5. to refresh the screen
     clock.tick(60)
-pygame.quit()
+#pygame.quit()
